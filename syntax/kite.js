8c1c60367a4467cfceac99a2697e0db67603b8ef
Subhajit Sahu <wolfram77@gmail.com>
2014-12-06 22:19:52 +0530

    added more ideas

--- syntax/kite.js
+++ syntax/kite.js
@@ -11,6 +11,7 @@
 "- distributed, fault tolerant, soft real-time, hot swapping, self modification, low level code"
 "- flip-flop operator, function-pointer table, remote procedure call, true OO, flow-control"
 "- simple io, named parameters, non-dangling else, lots of built-in datatypes, exception handling"
+"- side by side execution, edit and continue debugging, click and deploy"
 
 
 simple interface declaration in machine understood way
